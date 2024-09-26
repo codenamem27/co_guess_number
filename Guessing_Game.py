643d3e93598bb51fb9b3b2acc448df6bc32e8df2
codenamem27 <codenamem27@users.noreply.github.com>
2024-09-26 21:05:11 +1000

    Finished looping

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,17 +1,34 @@
 
 import random
 
-correct = False
-number = random.randint(1, 1000)
-while not correct:
-    guessed = input("What is your guess? ")
-    if guessed.isdigit():
-        if int(guessed) == number:
-            print("correct!")
-            correct = True
-        elif int(guessed) < number:
-            print("too low...")
-        elif int(guessed) > number:
-            print("too high...")
-    else:
-        print("error")
+def game():
+    correct = False
+    number = random.randint(1, 1000)
+    while not correct:
+        guessed = input("What is your guess? ")
+        if guessed.isdigit():
+            if int(guessed) == number:
+                print("correct!")
+                correct = True
+            elif int(guessed) < number:
+                print("too low...")
+            elif int(guessed) > number:
+                print("too high...")
+        else:
+            print("error")
+
+loop = True
+
+while loop:
+    game()
+    ask = True
+    while ask:
+        again = input("go again? answer y or n ")
+        if again == "y":
+            game()
+        elif again == "n":
+            loop = False
+            ask = False
+        else:
+            print("try again")
+print("goodbye!")
