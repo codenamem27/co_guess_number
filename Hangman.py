13dc7e9f66946ed221526d6e9224e3143f93310b
codenamem27 <codenamem27@users.noreply.github.com>
2024-10-04 17:22:54 +1000

    simple alg complete and hangman started

--- Hangman.py
+++ Hangman.py
@@ -1,13 +1,57 @@
 from wonderwords import RandomWord
 
-r = RandomWord()
-word = r.word(word_min_length=5, word_max_length=5)
-word_letters = list(word)
-print(word_letters)
-print("_ _ _ _ _")
-letter = input("guess a letter: ")
-if letter in word_letters:
-    print("horay")
-else:
-    print("awww...")
-    
+
+def checkLetter(letter, word, guess_word):
+    for c in word:
+        if c == letter:
+            guess_word[word.index(c)] = c
+            word[word.index(c)] = '*'
+
+    return guess_word
+
+def hangman_symbol(stage):
+    if stage < 7:
+        print("        ________________________")
+    else:
+        print("")
+    if stage < 8:
+        print("        |")
+    elif stage < 6:
+        print("        |                    |")
+    if stage < 8:
+        print("        |")
+    elif stage < 6:
+        print("        |                    |")
+        if stage < 8:
+            print("        |")
+        elif stage < 5:
+            print("        |		     _")
+            print("        |		   /   \\")
+            print("        |		   \\ _ /")
+
+
+
+
+
+
+word = list(RandomWord().word(word_min_length=5, word_max_length=5))
+
+guess_word = ['_' for x in word]
+word_display2 = ""
+lives = 8
+while '_' in guess_word:
+    guess = input('Guess a letter: ')
+    word_display = checkLetter(guess, word, guess_word)
+    print( ' '.join(word_display))
+
+    if word_display == word_display2:
+        hangman_symbol(lives)
+        lives -= 1
+
+    word_display2 = word_display
+
+    if lives == 0:
+        print("💀 you lost 💀")
+        print("The answer was " + word)
+
+
