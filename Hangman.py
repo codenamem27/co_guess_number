e6a6c826e541166c02c005588a162d6ba5335e75
codenamem27 <codenamem27@users.noreply.github.com>
2024-10-09 15:37:25 +1100

    Finished looping system

--- Hangman.py
+++ Hangman.py
@@ -10,6 +10,7 @@ def checkletter(letter, worded, guess_worded):
     return guess_worded
 
 def hangman_symbol(stage):
+    correct = True
     if stage == 10:
         print("\n\n\n\n\n\n\n")
         print("=========")
@@ -87,32 +88,50 @@ def hangman_symbol(stage):
         print("        |")
         print("=========")
 
-word = list(RandomWord().word(word_min_length=5, word_max_length=5))
-guessed = []
-print(word)
-guess_word = ['_' for x in word]
+def game():
+    word = list(RandomWord().word(word_min_length=5, word_max_length=5))
+    guessed = []
+    print(word)
+    guess_word = ['_' for x in word]
+    correct = True
+    print(" ".join(guess_word))
+    lives = 11
+    while '_' in guess_word:
 
-print(" ".join(guess_word))
-lives = 11
-while '_' in guess_word:
-
-    guess = input('Guess a letter: ')
-    print(" ".join(checkletter(guess, word, guess_word)))
-    if guess not in word:
-        lives -= 1
-        hangman_symbol(lives)
-    elif guess in guessed:
-        lives -= 1
-        hangman_symbol(lives)
-    else:
-        guessed.append(guess)
-    if lives == 0:
-        print("💀 you lost 💀")
-        print("The answer was " + ''.join(word))
+        guess = input('Guess a letter: ')
+        print(" ".join(checkletter(guess, word, guess_word)))
+        if guess not in word:
+            lives -= 1
+            hangman_symbol(lives)
+        elif guess in guessed:
+            lives -= 1
+            hangman_symbol(lives)
+        else:
+            guessed.append(guess)
+        if lives == 0:
+            print("💀 you lost 💀")
+            print("The answer was " + ''.join(word))
+            correct = False
 
 
+    if correct:
+        print("well done")
 
+play = True
+while play:
+    game()
 
+    check = True
+    while check :
+        again = input("play again? answer y or n: ")
+        if again == "y":
+            game()
+        elif again == "n":
+            play = False
+            check = False
+        else:
+            print("try again")
+print("Goodbye")
 
 
 
