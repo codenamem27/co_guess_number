b67e20eb7c28e42a1529c48a9b3311c7996cb2c7
codenamem27 <codenamem27@users.noreply.github.com>
2024-11-17 07:56:20 +1100

    made encoding and decoding

--- Cypher.py
+++ Cypher.py
@@ -1,110 +1,27 @@
-import random
-import textwrap
 
-def get_key_by_value(dictionary, value):
-    for key, val in dictionary.items():
-        if val == value:
-            return key
-    return None
+import base64
 
-cyphers = {
-    "a": 4477,
-    "b": 5476,
-    "c": 2349,
-    "d": 1420,
-    "e": 4757,
-    "f": 1956,
-    "g": 3132,
-    "h": 1967,
-    "i": 2548,
-    "j": 2213,
-    "k": 1165,
-    "l": 1113,
-    "m": 3713,
-    "n": 1304,
-    "o": 2545,
-    "p": 3576,
-    "q": 2919,
-    "r": 1864,
-    "s": 4116,
-    "t": 4443,
-    "u": 3067,
-    "v": 2203,
-    "w": 3301,
-    "x": 4817,
-    "y": 2653,
-    "z": 3238,
-    "!": 4797,
-    "@": 3139,
-    "#": 2424,
-    "$": 4680,
-    "%": 2155,
-    "^": 2972,
-    "&": 3448,
-    "*": 4924,
-    "(": 1569,
-    ")": 1565,
-    "'": 2289,
-    '"': 1739,
-    ":": 2482,
-    ";": 1360,
-    ",": 4483,
-    ".": 3557,
-    "<": 2448,
-    ">": 3049,
-    "?": 1104,
-    "/": 2534,
-    "\\": 4312,
-    "_": 4438,
-    "-": 3228,
-    "+": 3232,
-    "9": 2366,
-    "8": 4762,
-    "7": 3622,
-    "6": 3332,
-    "5": 4561,
-    "4": 2532,
-    "3": 3058,
-    "2": 2637,
-    "1": 4367,
-    "0": 2605,
-    "=": 4512,
-    " ": 2354,
-    "[": 4134,
-    "]": 3214,
-    "{": 4123,
-    "}": 5123,
-    "|": 4332,
-    "π": 5143
-}
+
 
 def encrypt(encryption):
-    number = random.randint(1000, 4000)
-    output = []
-
-    for char in encryption:
-        if char in cyphers:
-            output.append(str(cyphers[char] + number))
-        else:
-            return"This phrase has produced an error"
+    data_bytes = encryption.encode('utf-8')
+    encoded_data = base64.b64encode(data_bytes)
+    encoded_string = encoded_data.decode('utf-8')
+    print(encoded_string)
 
 
 
-    return str(number) + "".join(output)
 
 
-def decrypt(numbers, key):
-    decryption = textwrap.wrap(numbers, 4)
 
-    output = []
+def decrypt(numbers):
+    try:
+        decoded_data = base64.b64decode(numbers)
+        decoded_string = decoded_data.decode('utf-8')
+        print(decoded_string)
+    except Exception as e:
+        print("This phrase has caused an error")
 
-    for item in decryption:
-        item_no = int(item) - int(key)
-        output.append(get_key_by_value(cyphers, item_no))
-    if len(output) < 4:
-        print("Your phrase made an error")
-    else:
-        print(''.join(output))
 
 loop = True
 need_loop = True
@@ -115,8 +32,7 @@ while loop:
         _crypt = input("e for encryption or d for decryption: ")
         if _crypt == "e":
             phrase1 = input("insert phrase: ")
-            phrase = phrase1.lower()
-            print(encrypt(phrase))
+            encrypt(phrase1)
             looping = True
             while looping:
                 again = input("use again? Answer 'y' for yes and 'n' for no: ")
@@ -137,10 +53,9 @@ while loop:
             while check:
                 phrase1 = input("insert phrase: ")
                 check = False
-            phrase = phrase1[4:]
-            first = phrase1[:4]
 
-            decrypt(phrase, first)
+
+            decrypt(phrase1)
             looping = True
             while looping:
                 again = input("use again? Answer 'y' for yes and 'n' for no: ")
