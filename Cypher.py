7502db574ef6cbebf18e15ec301807838c11bf77
codenamem27 <codenamem27@users.noreply.github.com>
2024-11-19 18:17:25 +1100

    Finished encoding

--- Cypher.py
+++ Cypher.py
@@ -7,16 +7,19 @@ def encrypt(encryption):
     data_bytes = encryption.encode('utf-8')
     encoded_data = base64.b64encode(data_bytes)
     encoded_string = encoded_data.decode('utf-8')
-    print(encoded_string)
+    return encoded_string
+
+def encode(phrase):
+    listed = list(phrase)
+    return (''.join(reversed(listed)))
 
 
 
 
-
-
-def decrypt(numbers):
+def decrypt(number1):
+    numbers = list(reversed(number1))
     try:
-        decoded_data = base64.b64decode(numbers)
+        decoded_data = base64.b64decode(''.join(numbers))
         decoded_string = decoded_data.decode('utf-8')
         print(decoded_string)
     except Exception as e:
@@ -32,7 +35,7 @@ while loop:
         _crypt = input("e for encryption or d for decryption: ")
         if _crypt == "e":
             phrase1 = input("insert phrase: ")
-            encrypt(phrase1)
+            print(encode(encrypt(phrase1)))
             looping = True
             while looping:
                 again = input("use again? Answer 'y' for yes and 'n' for no: ")
